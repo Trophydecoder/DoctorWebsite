dd23aa12e234fbfac3f18e3d9ab61fe81780d41a
Trophydecoder <trophydecoder@gmail.com>
2025-01-13 22:47:18 +0200

    application booking for

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    BrowserModule,
+    FormsModule // Add FormsModule here
   ],
   bootstrap: [AppComponent],
 })
--- src/app/components/global/home/bookings/bookings.component.ts
+++ src/app/components/global/home/bookings/bookings.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './bookings.component.scss'
 })
 export class BookingsComponent {
-
 }
+  
